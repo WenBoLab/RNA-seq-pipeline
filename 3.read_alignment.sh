c27571c0a28f6643a7dfe4fa5cef2abf9fa9270b
123zelia <149145305+123zelia@users.noreply.github.com>
2026-01-26 10:50:22 +0800

    Refactor RNA-seq alignment script with STAR

--- 3.read_alignment.sh
+++ 3.read_alignment.sh
@@ -1,40 +1,82 @@
 #!/bin/bash
 set -euo pipefail
 
-# set the path
-reference_dir="/data/RNA-seq/reference/mouse"
-reference_index_dir="/data/RNA-seq/reference/mouse/bowtie2Index"
-trim_dir="/data/RNA-seq/01/results/trim"
-alignment_dir="/data/RNA-seq/01/results/bowtie2"
+# ========= global parameters =========
+SAMPLES=$1          # eg.：SRR12043508,SRR12043509,SRR18909870,SRR18909871
+CONFIG=$2           # RNA-seq_config.sh
 
-# use conda environment
-source /data/software/miniconda3/etc/profile.d/conda.sh
-conda activate RNA_env
+# ========= config =========
+source ${CONFIG}
+
+# ========= conda =========
+set +u
+source ${CONDA_BASE}/etc/profile.d/conda.sh
+conda activate ${CONDA_ENV}
+set -u
 
 # create folders
-mkdir -p ${alignment_dir}
-mkdir -p ${alignment_dir}/sam
-mkdir -p ${alignment_dir}/sam/bowtie2_summary
-mkdir -p ${alignment_dir}/bam
+mkdir -p "${alignment_dir}"
 
-# build index
-gunzip ${reference_dir}/Mus_musculus.GRCm39.dna.primary_assembly.fa.gz
-bowtie2-build ${reference_dir}/Mus_musculus.GRCm39.dna.primary_assembly.fa  ${reference_index_dir}/GRCm39.genome.mm
+echo "start running STAR..."
+
+IFS=',' read -ra SAMPLE_ARRAY <<< "${SAMPLES}"
+
+fa_gz="${reference_dir}/hg19.fa.gz"
+fa="${reference_dir}/hg19.fa"
+
+gtf_gz="${GTF_dir}/gencode.v19.annotation.gtf.gz"
+gtf="${GTF_dir}/gencode.v19.annotation.gtf"
+
+# parameter -f: if fa_gz file path exists
+if [[ -f "$fa_gz" && ! -f "$fa" ]]; then
+    echo "Unzipping genome fasta..."
+    gunzip -k "$fa_gz"
+else
+    echo "Genome fasta already unzipped, skip."
+fi
+
+if [[ -f "$gtf_gz" && ! -f "$gtf" ]]; then
+    echo "Unzipping GTF..."
+    gunzip -k "$gtf_gz"
+else
+    echo "GTF already unzipped, skip."
+fi
+
+# create index
+index_dir="${reference_dir}/hg19_Index"
+# parameter -d: if the index_dir path exists
+if [[ ! -d "$index_dir" ]]; then
+    echo "create STAR index..."
+    mkdir -p "${index_dir}"
+    STAR --runMode genomeGenerate \
+    --runThreadN ${THREADS} \
+    --genomeDir ${index_dir} \
+    --genomeFastaFiles ${reference_dir}/hg19.fa \
+    --sjdbGTFfile "${GTF_dir}/gencode.v19.annotation.gtf" \
+    --sjdbOverhang 149
+else
+    echo "index already exist, skip."
+fi
 
 
-# mapping
-for i in {12..19};do
-    bowtie2 \
-    --local --very-sensitive --no-mixed --no-discordant --phred33 \
-    -I 10 -X 700 -p 4 \
-    --rg-id CDNA"$i" --rg SM:CDNA"$i" \
-    -x ${reference_index_dir}/GRCm39.genome.mm \
-    -1 ${trim_dir}/CDNA${i}_1_val_1.fq.gz \
-    -2 ${trim_dir}/CDNA${i}_2_val_2.fq.gz \
-    -S ${alignment_dir}/sam/CDNA${i}_bowtie2.sam &> ${alignment_dir}/sam/bowtie2_summary/CDNA${i}_bowtie2.txt   
-    # sam to bam  
-    samtools view -bS -F 0x04 ${alignment_dir}/sam/CDNA${i}_bowtie2.sam -o ${alignment_dir}/bam/RCDNA${i}_bowtie2.bam  
-    samtools sort ${alignment_dir}/bam/CDNA${i}_bowtie2.bam -o ${alignment_dir}/bam/CDNA${i}_bowtie2.sort.bam
-    samtools index ${alignment_dir}/bam/CDNA${i}_bowtie2.sort.bam
+for i in "${SAMPLE_ARRAY[@]}"; do
+  echo ">>> Mapping sample: ${i}.clean.fq.gz"
 
-done
+  STAR --runThreadN ${THREADS} \
+       --genomeDir ${index_dir} \
+       --readFilesIn "${clean_dir}/${i}_1_clean.fq.gz" "${clean_dir}/${i}_2_clean.fq.gz" \
+       --readFilesCommand zcat \
+       --outFileNamePrefix "${alignment_dir}/${i}." \
+       --twopassMode Basic \
+       --sjdbGTFfile "${GTF_dir}/gencode.v19.annotation.gtf" \
+       --quantMode  GeneCounts \
+       --outSAMtype BAM SortedByCoordinate \
+       --outSAMattributes NH HI AS nM XS \
+       --outFilterMatchNminOverLread 0.3 \
+       --outFilterScoreMinOverLread 0.3 \
+       --outSAMattrRGline ID:$i SM:$i PL:ILLUMINA
+
+  # create BAM index
+  samtools index "${alignment_dir}/${i}.Aligned.sortedByCoord.out.bam"
+  mv "${alignment_dir}/${i}.Log.final.out" "${alignment_dir}/${i}.Log.final.out.tsv"
+#done
