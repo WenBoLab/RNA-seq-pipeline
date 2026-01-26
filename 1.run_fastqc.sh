b106ab0655eed190b9c76f6a89efe35bea69d73a
123zelia <149145305+123zelia@users.noreply.github.com>
2026-01-26 10:39:26 +0800

    Enhance 1.run_fastqc.sh with usage comments
    Added usage instructions and argument details for the script.

--- 1.run_fastqc.sh
+++ 1.run_fastqc.sh
@@ -1,1 +1,10 @@
-bash 1.data_quality_control.sh "SRR12043508,SRR12043509,SRR18909870,SRR18909871" RNA-seq_config.sh
+bash 1.data_quality_control.sh \
+"SRR12043508,SRR12043509,SRR18909870,SRR18909871" \
+RNA-seq_config.sh
+
+# Usage:
+#   bash 1.data_quality_control.sh <sample_ids> <config_file>
+
+# Arguments:
+#   sample_ids   A comma-separated list of SRA accession IDs (e.g. SRR12043508,SRR12043509)
+#   config_file  Configuration file specifying paths and parameters for RNA-seq analysis
