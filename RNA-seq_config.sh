f22f726337a92ffb500c8fae04babc9f2c6a2bed
123zelia <149145305+123zelia@users.noreply.github.com>
2026-01-26 10:31:54 +0800

    Update directory paths in RNA-seq configuration

--- RNA-seq_config.sh
+++ RNA-seq_config.sh
@@ -1,21 +1,21 @@
-# ========= 基础路径 =========
-data_dir="/data/xiejing/liuhao/RNA-seq/03/fastq_data"
-fastqc_dir="/data/xiejing/liuhao/RNA-seq/03/results/fastqc"
-multiqc_dir="/data/xiejing/liuhao/RNA-seq/03/results/multiqc"
+# ========= base directory =========
+data_dir="/data/RNA-seq/01/fastq_data"
+fastqc_dir="/data/RNA-seq/01/results/fastqc"
+multiqc_dir="/data/RNA-seq/01/results/multiqc"
 
-clean_dir="/data/xiejing/liuhao/RNA-seq/03/results/fastp"
-clean_fastqc_dir="/data/xiejing/liuhao/RNA-seq/03/fastp/fastqc"
-clean_multiqc_dir="/data/xiejing/liuhao/RNA-seq/03/fastp/multiqc"
+clean_dir="/data/RNA-seq/03/results/fastp"
+clean_fastqc_dir="/data/RNA-seq/01/fastp/fastqc"
+clean_multiqc_dir="/data/RNA-seq/01/fastp/multiqc"
 
-reference_dir="/data/xiejing/liuhao/RNA-seq/reference/human/hg19"
-alignment_dir="/data/xiejing/liuhao/RNA-seq/03/results/map"
+reference_dir="/data/RNA-seq/reference/human/hg19"
+alignment_dir="/data/RNA-seq/01/results/map"
 
-# ========= 运行参数 =========
+# ========= parameters =========
 THREADS=8
 INSERT_MIN=10
 INSERT_MAX=700
 
 # ========= conda =========
-CONDA_BASE="/data/xiejing/software/miniconda3"
+CONDA_BASE="/data/software/miniconda3"
 CONDA_ENV="RNA_env"
 
