921cc9bc5c9a4132005ae2994b94dfe6bb09b036
123zelia <149145305+123zelia@users.noreply.github.com>
2026-01-26 10:43:27 +0800

    Revise usage and argument documentation in script
    Updated usage instructions and added argument details.

--- 1.run_fastqc.sh
+++ 1.run_fastqc.sh
@@ -3,8 +3,9 @@ bash 1.data_quality_control.sh \
 RNA-seq_config.sh
 
 # Usage:
-#   bash 1.data_quality_control.sh <sample_ids> <config_file>
+#   bash <program_file> <sample_ids> <config_file>
 
 # Arguments:
+#   program_file   The executable shell script to be run (e.g. 1.data_quality_control.sh, 2.read_trimming_and_filtering.sh)
 #   sample_ids   A comma-separated list of SRA accession IDs (e.g. SRR12043508,SRR12043509)
 #   config_file  Configuration file specifying paths and parameters for RNA-seq analysis
