e7856543ca99c177ebd48c5a6ce1eb39cf124d53
123zelia <149145305+123zelia@users.noreply.github.com>
2026-01-26 10:48:44 +0800

    Refactor read trimming and filtering script

--- 2.read_trimming_and_filtering.sh
+++ 2.read_trimming_and_filtering.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 set -euo pipefail
 
-# set the path
-data_dir="/data/RNA-seq/01/data"
-trim_dir="/data/RNA-seq/01/results/trim"
-trim_fastqc="/data/RNA-seq/01/results/trim/fastqc"
-trim_multiqc="/data/RNA-seq/01/results/trim/multiqc"
-# use conda environment
-source /data/software/miniconda3/etc/profile.d/conda.sh
-conda activate RNA_env
+# ========= global parameters =========
+SAMPLES=$1          # eg.：SRR12043508,SRR12043509,SRR18909870,SRR18909871
+CONFIG=$2           # RNA-seq_config.sh
+
+# ========= config =========
+source ${CONFIG}
+
+# ========= conda =========
+set +u
+source ${CONDA_BASE}/etc/profile.d/conda.sh
+conda activate ${CONDA_ENV}
+set -u
 
 # create folders
-mkdir -p "${trim_dir}"
+mkdir -p "${clean_dir}"
 
-# step1. Read trimming and filtering
-for i in {12..19};do
-    trim_galore --paired --cores 4 \
-      -o ${trim_dir} \
-      ${data_dir}/CDNA${i}/CDNA${i}_1.fq.gz \
-      ${data_dir}/CDNA${i}/CDNA${i}_2.fq.gz
+echo "start running trim_galore..."
+# step1. fastqc for raw quality control
+IFS=',' read -ra SAMPLE_ARRAY <<< "${SAMPLES}"
+
+for i in "${SAMPLE_ARRAY[@]}"; do
+    fastp -i ${data_dir}/${i}/"$i"_1.fq.gz -o ${clean_dir}/"$i"_1_clean.fq.gz \
+    -I ${data_dir}/${i}/"$i"_2.fq.gz -O ${clean_dir}/"$i"_2_clean.fq.gz \
+    -q 20 -u 30 -n 5 -l 30 --thread ${THREADS} --html ${clean_dir}/${i}.fastp.html
 done
-# step2. fastqc after filtering
 
-mkdir -p "${trim_fastqc}" "${trim_multiqc}"
-fastqc -t 4 -o ${trim_fastqc}/ -f fastq ${trim_dir}/*.fq.gz
-multiqc ${trim_fastqc}/ -o ${trim_multiqc}/
+# step2. fastqc after filtering
+echo "start running FastQC after clean..."
+mkdir -p "${clean_fastqc_dir}" "${clean_multiqc_dir}"
+fastqc -t ${THREADS} -o ${clean_fastqc_dir}/ -f fastq ${clean_dir}/*.fq.gz
+echo "start running MultiQC after clean..."
+multiqc ${clean_fastqc_dir}/ -o ${clean_multiqc_dir}/
