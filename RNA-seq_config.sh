bd207a4352a1873eecd094b5ba4dd1f8ef279143
123zelia <149145305+123zelia@users.noreply.github.com>
2026-01-26 10:33:17 +0800

    Update directory paths in RNA-seq configuration

--- RNA-seq_config.sh
+++ RNA-seq_config.sh
@@ -3,12 +3,15 @@ data_dir="/data/RNA-seq/01/fastq_data"
 fastqc_dir="/data/RNA-seq/01/results/fastqc"
 multiqc_dir="/data/RNA-seq/01/results/multiqc"
 
-clean_dir="/data/RNA-seq/03/results/fastp"
-clean_fastqc_dir="/data/RNA-seq/01/fastp/fastqc"
-clean_multiqc_dir="/data/RNA-seq/01/fastp/multiqc"
+clean_dir="/data/RNA-seq/01/results/fastp"
+clean_fastqc_dir="/data/RNA-seq/01/results/fastp/fastqc"
+clean_multiqc_dir="/data/RNA-seq/01/results/fastp/multiqc"
 
 reference_dir="/data/RNA-seq/reference/human/hg19"
+GTF_dir="/data/RNA-seq/reference/human/hg19"
 alignment_dir="/data/RNA-seq/01/results/map"
+
+counts_dir="/data/RNA-seq/01/results/counts"
 
 # ========= parameters =========
 THREADS=8
