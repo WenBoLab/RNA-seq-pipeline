0497024814510baad6d729e0e990794c23df6758
123zelia <149145305+123zelia@users.noreply.github.com>
2026-07-20 10:23:29 +0800

    Add script to convert SRA files to FASTQ format
    This script converts SRA files to FASTQ files using fastq-dump. It checks for the existence of SRA files and creates necessary directories for output.

---
+++ 0.sra2fastq.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eo pipefail
+
+# ========= global parameters =========
+SAMPLES=$1          # eg.：SRR12043508,SRR12043509,SRR18909870,SRR18909871
+CONFIG=$2           # RNA-seq_config.sh
+
+# ========= config =========
+source "${CONFIG}"
+
+# ========= conda =========
+set +u
+source ${CONDA_BASE}/etc/profile.d/conda.sh
+conda activate "${CONDA_ENV}"
+set -u
+
+echo "Convert SRA Files to FASTQ Files..."
+IFS=',' read -ra SAMPLE_ARRAY <<< "${SAMPLES}"
+
+for i in "${SAMPLE_ARRAY[@]}"; do
+
+    if [[ ! -f "${data_dir}/${i}/${i}.sra" ]]; then
+        echo "Error: ${data_dir}/${i}/${i}.sra not found!"
+        exit 1
+    fi
+
+	mkdir -p "${data_dir}/fastq_data/${i}"
+	
+	${sratoolkit_dir}/fastq-dump --gzip --split-3 "${data_dir}/${i}/${i}.sra" -O ${data_dir}/fastq_data/${i}/
+done
