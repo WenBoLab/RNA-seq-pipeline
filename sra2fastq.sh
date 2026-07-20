32cc283cf9e35135d645e24afd283c2c9da95b47
123zelia <149145305+123zelia@users.noreply.github.com>
2026-07-20 10:21:13 +0800

    Enhance sra2fastq.sh for conda and error handling
    Updated the script to include conda environment activation and improved error handling for missing SRA files.

--- sra2fastq.sh
+++ sra2fastq.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 set -eo pipefail
 
-source /data/shaolizhen/software/miniconda3/etc/profile.d/conda.sh
-conda activate RNA_env
+# ========= global parameters =========
+SAMPLES=$1          # eg.：SRR12043508,SRR12043509,SRR18909870,SRR18909871
+CONFIG=$2           # RNA-seq_config.sh
 
-data_dir="/data/shaolizhen/crosstalk/NAFLD/data/mouse/GSE165752"
-software_dir="/data/shaolizhen/software/sratoolkit.3.2.1-ubuntu64/bin"
-for i in 79 80 81 82 87 88 89 90;do
-	${software_dir}/fastq-dump --gzip --split-3 ${data_dir}/SRR135756"$i"/SRR135756"$i".sra -O ${data_dir}/fastq_data/SRR135756"$i"/
+# ========= config =========
+source "${CONFIG}"
+
+# ========= conda =========
+set +u
+source ${CONDA_BASE}/etc/profile.d/conda.sh
+conda activate "${CONDA_ENV}"
+set -u
+
+echo "Convert SRA Files to FASTQ Files..."
+IFS=',' read -ra SAMPLE_ARRAY <<< "${SAMPLES}"
+
+for i in "${SAMPLE_ARRAY[@]}"; do
+
+    if [[ ! -f "${data_dir}/${i}/${i}.sra" ]]; then
+        echo "Error: ${data_dir}/${i}/${i}.sra not found!"
+        exit 1
+    fi
+
+	mkdir -p "${data_dir}/fastq_data/${i}"
+	
+	${sratoolkit_dir}/fastq-dump --gzip --split-3 "${data_dir}/${i}/${i}.sra" -O ${data_dir}/fastq_data/${i}/
 done
 
 
